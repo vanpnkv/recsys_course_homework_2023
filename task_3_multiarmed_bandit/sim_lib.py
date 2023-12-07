b4dd7a2c271f155520316e980edfc0f0a54cbae6
Julia Belova <rollineea@gmail.com>
2023-12-07 19:34:53 +0100

    add hw03

--- task_3_multiarmed_bandit/sim_lib.py
+++ task_3_multiarmed_bandit/sim_lib.py
@@ -1,9 +1,9 @@
 
+from typing import Callable
+
 import numpy as np
 import pandas as pd
-
-from scipy.stats import beta, uniform, bernoulli, expon
-from typing import Callable
+from scipy.stats import bernoulli, beta, expon, uniform
 
 ALPHA = 1
 BETA = 20
@@ -21,7 +21,7 @@ def generate_new_banner(n, a=ALPHA, b=BETA, mu=MU, random_state=None):
     return p, lifetimes
 
 
-def simulation(policy: Callable, n=10 ** 6, initial_banners=9, seed=None):
+def simulation(policy: Callable, n=10 ** 6, initial_banners=9, seed=None, verbose=True):
     state = pd.DataFrame(np.zeros((initial_banners, 4)), columns=['impressions', 'clicks', 'lifetime', 'p'])
     state['p'], state['lifetime'] = generate_new_banner(initial_banners)
     regret = 0
@@ -51,7 +51,7 @@ def simulation(policy: Callable, n=10 ** 6, initial_banners=9, seed=None):
         if random_state:
             random_state = 7*random_state % MAX_RANDOM
 
-        if not i % MONITORING_FREQ:
+        if verbose and not i % MONITORING_FREQ:
             print('{} impressions have been simulated'.format(i + 1))
 
     return {'regret': regret, 'rounds': n, 'total_banners': max_index, 'history': state}
